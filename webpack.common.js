7b47a8254bcdc0b91f4b5c649458ef9609d246e5
itixyxywa <ukv220@gmail.com>
2023-10-25 00:56:03 +0500

    initialized project

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: './src/index.tsx',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'babel-loader',
+				options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+				exclude: /(node_modules|bower_components)/,
+			},
+
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(tsx|ts)$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif)$/i,
+				use: ['file-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: [__dirname, '*', '.js', '.jsx', '.ts', '.tsx'],
+		alias: {
+			'@app': path.resolve(__dirname, 'src/app'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@entities': path.resolve(__dirname, 'src/entities'),
+			extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+		},
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist/'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+}
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		port: 3000,
+		historyApiFallback: true,
+		compress: true,
+		open: true,
+	},
+})
